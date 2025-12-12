094d2bc41aeef97f9ad22c51c6294f2fc02a5174
Vibby Dude <sentinalsense@gmail.com>
2025-12-12 11:52:30 -0500

    Day 12

--- day12/main.py
+++ day12/main.py
@@ -1,4 +1,4 @@
 inp, blocks, regions = open("input.txt", "r").read().strip().split("\n\n"), [], []
 [blocks.append(tuple([(i,j) for i,ls in enumerate(entry.split(":\n")[1].split("\n")) for j,v in enumerate(ls) if v == "#"])) if ":\n" in entry else [regions.append((tuple(map(int, region.split(": ")[0].split("x"))), tuple([int(x) for x in region.split(": ")[1].split(" ")]))) for region in entry.split("\n")] for entry in inp]
 
-print(f"Part 1: {sum([x*y>=7*sum(z) for ((x,y), z) in regions])}")
+print(f"Part 1: {sum([x*y>=9*sum(z) for ((x,y), z) in regions])}")
